5a11b5bfff2c835a67774cbe039fdbe7f525018c
chillz-the-first <vhenganitshilidzi846@gmail.com>
2026-08-20 21:05:19 +0200

    completed the tweet method

--- main.py
+++ main.py
@@ -15,13 +15,14 @@ class InternetSpeedBot:
         self.email = os.environ.get('Y_EMAIL')
         self.password = os.environ.get('Y_PASSWORD')
         self.url = os.environ.get('Y_LOGIN_URL')
-        self.down = down
-        self.up = up
+        self.down = float(down)
+        self.up = float(up)
         self.driver = webdriver.Chrome()
         self.wait = WebDriverWait(self.driver, 10)
 
     def login(self):
         try:
+            print('Logging in...')
             self.driver.get(self.url)
 
             email_input = self.wait.until(EC.element_to_be_clickable((By.NAME, "email")))
@@ -39,6 +40,7 @@ class InternetSpeedBot:
 
     def get_internet_speed(self):
         try:
+            print('Getting Internet Speed...')
             self.driver.get(os.environ.get("TEST_URL"))
 
             start_btn = self.wait.until(EC.element_to_be_clickable(
@@ -46,26 +48,39 @@ class InternetSpeedBot:
             ))
             start_btn.click()
 
-            time.sleep(45)
+            long_wait = WebDriverWait(self.driver, 60)
+            results = long_wait.until(EC.visibility_of_element_located(
+                (By.XPATH, '//*[@id="root"]/div/div[1]/div/div[2]/div[2]/div[2]/div/div/div/div[1]/p')
+            ))
 
-            download = self.wait.until(EC.visibility_of_element_located(
+            download = long_wait.until(EC.visibility_of_element_located(
                 (By.XPATH,
                  '//*[@id="root"]/div/div[1]/div/div[2]/div[2]/div[2]/div/div/div/div[2]/div[2]/div[1]/div[1]/div/h3')
             )).text
-            upload = self.wait.until(EC.visibility_of_element_located(
+            download = float(download)
+            download = float(download)
+            upload = long_wait.until(EC.visibility_of_element_located(
                 (By.XPATH,
                  '//*[@id="root"]/div/div[1]/div/div[2]/div[2]/div[2]/div/div/div/div[2]/div[2]/div[1]/div[2]/div/h3')
             )).text
+            upload = float(upload)
 
-            print("Download Speed:", download)
-            print(type(download))
-            print("Upload Speed:", upload)
-            print(type(upload))
+            print(f"Internet Speed: {download} / {upload}")
+            return download, upload
         except TimeoutException:
             print("Speed test failed")
 
-    def tweet_at_provider(self):
-        pass
+    def tweet_at_provider(self, d_speed, u_speed):
+        print("Tweeting at provider")
+        if self.down > d_speed or self.up > u_speed:
+            msg = (f"Hey @vodacom, why is my internet speed {d_speed}Mbps down/{u_speed}Mbps up"
+                   f"when I pay for {self.down}Mbps down/{self.up}Mbps up")
+
+            post = self.wait.until(EC.element_to_be_clickable((By.XPATH, '//*[@id="tweet-compose"]')))
+            post.send_keys(msg)
+
+            self.wait.until(EC.element_to_be_clickable((By.XPATH, '//*[@id="post-btn"]'))).click()
+            print("Tweet posted")
 
 
 promised_down = os.environ.get('PROMISED_DOWN')
@@ -73,9 +88,9 @@ promised_up = os.environ.get('PROMISED_UP')
 
 try:
     bot = InternetSpeedBot(down=promised_down, up=promised_up)
+    d_load, u_load = bot.get_internet_speed()
     bot.login()
-    bot.get_internet_speed()
-
+    bot.tweet_at_provider(d_speed=d_load, u_speed=u_load)
     time.sleep(20)
 finally:
     bot.driver.quit()
